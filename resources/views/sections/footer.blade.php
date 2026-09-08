e1b5f9bb24a99c479c087df182d0ac0101d8aca8
Eldo Magan <magan.eldo@gmail.com>
2026-09-08 22:13:59 +0100

    style: use full opacity footer text color

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -3,7 +3,7 @@
   $contentWidthClass = $section->settings->content_width === 'container' ? 'container' : 'container-fluid';
 @endphp
 
-<div {{ $section->editor_attributes }} class="bg-surface-alt text-on-surface-alt/80">
+<div {{ $section->editor_attributes }} class="bg-surface-alt text-on-surface-alt">
   <div class="{{ $contentWidthClass }}">
     <div class="{{ $paddingClasses }}">
       @children
